192ec0bd4728417bb6a0b89516ac5373b9f1982e
ankitsharma07 <ankitsharma0711@gmail.com>
2018-12-10 22:31:18 +0530

    new

---
+++ Question3.py
@@ -0,0 +1,11 @@
+"""
+Read an integer N.
+Without using any string methods, try to print the following:
+123...N
+
+Note that "..." represents the values in between.
+"""
+
+a = int(input())
+for i in range(1,a+1):
+    print(i, end='')
